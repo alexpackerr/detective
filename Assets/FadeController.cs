b50403c037d6a424731d73f7a99a5d4f6b56186d
NotALobster <92136499+NotALobster@users.noreply.github.com>
2022-10-01 12:38:58 -0700

    Fade in-out and grayscale toggles
    Right now just use Q, E, R to try the different methods.
    I attached them to yarn scripts so we can toggle characters.

---
+++ Assets/FadeController.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class FadeController : MonoBehaviour
+{
+
+    private bool fadeIn, fadeOut, talking;
+    [SerializeField]
+    private float fadeSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            fadeIn = true;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            fadeOut = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            talking = !talking;
+        }
+
+        if (fadeOut)
+        {
+            Color objectColor = this.GetComponent<SpriteRenderer>().color;
+            float fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
+            objectColor = new Color(objectColor.r, objectColor.b, objectColor.g, fadeAmount);
+            this.GetComponent<SpriteRenderer>().color = objectColor;
+
+            if (objectColor.a <= 0)
+            {
+                fadeOut = false;
+            }
+        }
+        else if (fadeIn)
+        {
+            Color objectColor = this.GetComponent<SpriteRenderer>().color;
+            float fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+            objectColor = new Color(objectColor.r, objectColor.b, objectColor.g, fadeAmount);
+            this.GetComponent<SpriteRenderer>().color = objectColor;
+            Debug.Log(this.GetComponent<SpriteRenderer>().color);
+            if (objectColor.a >= 1)
+            {
+                fadeIn = false;
+            }
+        }
+        else if(talking)
+        {
+            Color objectColor = this.GetComponent<SpriteRenderer>().color;
+            this.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, objectColor.a);
+        }
+        else if(!talking)
+        {
+            Color objectColor = this.GetComponent<SpriteRenderer>().color;
+            this.GetComponent<SpriteRenderer>().color = new Color(0.5f, 0.5f, 0.5f, objectColor.a);
+        }
+
+    }
+
+    [YarnCommand("FadeOut")]
+    public void FadeOut()
+    {
+        fadeOut = true;
+    }
+
+    [YarnCommand("FadeIn")]
+    public void FadeIn()
+    {
+        fadeIn = true;
+    }
+
+    [YarnCommand("StartTalk")]
+    public void StartTalk()
+    {
+        talking = true;
+    }
+    [YarnCommand("StopTalk")]
+    public void StopTalk()
+    {
+        talking = false;
+    }
+}
